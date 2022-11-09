b0512ae3a7850ab352baf78c53c18c5b8d00130f
UlfetZakirli <ulfetzakirli@gmail.com>
2022-11-09 20:13:31 +0400

    twitter-app: SidebarLink added to Sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,15 +1,25 @@
-import React from 'react'
-import  Image from 'next/image';
+import React from "react";
+import Image from "next/image";
+import SidebarLink from "./SidebarLink";
 
 const Sidebar = () => {
-    return (
-        <div className='hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full'>
-            <div className='flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24'>
-                <Image src="https://rb.gy/ogau5a" width={30}  height={30}/>
-            </div>
-            <div></div>
-        </div>
-    )
-}
+  return (
+    <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
+      <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
+        <Image src="https://rb.gy/ogau5a" width={30} height={30} />
+      </div>
+      <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
+        <SidebarLink text="Home" Icon={HomeIcon} active />
+        <SidebarLink text="Explore" Icon={HashtagIcon} active />
+        <SidebarLink text="Notifications" Icon={BellIcon} active />
+        <SidebarLink text="Messages" Icon={InboxIcon} active />
+        <SidebarLink text="Bookmarks" Icon={BookmarkIcon} active />
+        <SidebarLink text="Lists" Icon={ClipboardIcon} active />
+        <SidebarLink text="Profie" Icon={UserIcon} active />
+        <SidebarLink text="More" Icon={DotsCirlceHorizontalIcon} active />
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
---
+++ components/SidebarLink.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SidebarLink = () => {
+  return (
+    <div>SidebarLink</div>
+  )
+}
+
+export default SidebarLink
