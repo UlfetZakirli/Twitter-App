51b01737a8f465005fedd326e77a6122dbba8e21
UlfetZakirli <ulfetzakirli@gmail.com>
2022-11-09 21:54:38 +0400

    twitter-app: tweet button added and styled

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import SidebarLink from "./SidebarLink";
-import { HomeIcon } from '@heroicons/react/solid';
+import { HomeIcon } from "@heroicons/react/solid";
 import {
     HashtagIcon,
     BellIcon,
@@ -11,26 +11,43 @@ import {
     UserIcon,
     DotsCircleHorizontalIcon,
     DotsHorizontalIcon,
-  } from "@heroicons/react/outline/";
+} from "@heroicons/react/outline/";
 
 const Sidebar = () => {
-  return (
-    <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
-      <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
-        <Image src="https://rb.gy/ogau5a" width={30} height={30} />
-      </div>
-      <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
-        <SidebarLink text="Home" Icon={HomeIcon} active />
-        <SidebarLink text="Explore" Icon={HashtagIcon} />
-        <SidebarLink text="Notifications" Icon={BellIcon} />
-        <SidebarLink text="Messages" Icon={InboxIcon} />
-        <SidebarLink text="Bookmarks" Icon={BookmarkIcon} />
-        <SidebarLink text="Lists" Icon={ClipboardListIcon} />
-        <SidebarLink text="Profile" Icon={UserIcon} />
-        <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
-      </div>
-    </div>
-  );
+    return (
+        <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
+            <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
+                <Image src="https://rb.gy/ogau5a" width={30} height={30} />
+            </div>
+            <div className="space-y-.5 mt-3 mb-2.5 xl:ml-24">
+                <SidebarLink text="Home" Icon={HomeIcon} active />
+                <SidebarLink text="Explore" Icon={HashtagIcon} />
+                <SidebarLink text="Notifications" Icon={BellIcon} />
+                <SidebarLink text="Messages" Icon={InboxIcon} />
+                <SidebarLink text="Bookmarks" Icon={BookmarkIcon} />
+                <SidebarLink text="Lists" Icon={ClipboardListIcon} />
+                <SidebarLink text="Profile" Icon={UserIcon} />
+                <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
+            </div>
+            <button
+                className="hidden xl:inline ml-auto bg-[#1d9bf0] text-white
+                 rounded-full w-56 h-[40px] text-lg font-bold shadow-md hover:bg-[#1a8cd8]"
+            >
+                Tweet
+            </button>
+            <div className="text-[#d9d9d9] flex items-center justify-center hoverAnimation xl:ml-auto xl:-mr-5 mt-auto">
+                <img
+                    src="https://i.pinimg.com/564x/b3/26/b5/b326b5f8d23cd1e0f18df4c9265416f7--facebook-icon-negative-feedback.jpg"
+                    alt=""
+                    className="h-10 w-10 rounded-full xl:mr-2.5"
+                />
+                <div className="hidden xl:inline leading-5">
+                    <h4 className="font-bold">firebase 1903</h4>
+                    <p className="text-[#6e767d]">firebase 1903</p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Sidebar;
--- components/SidebarLink.jsx
+++ components/SidebarLink.jsx
@@ -3,9 +3,10 @@ import React from "react";
 const SidebarLink = ({ text, Icon, active }) => {
   return (
     <div
-      className={`text-[#d9d9d9] flex items-center justify-center xl:justify-start text-xl space-x-3 hoverAnimation`}
+      className={`text-[#d9d9d9] flex items-center justify-center xl:justify-start 
+       space-x-3 hoverAnimation ${active && "font-bold"}`}
     >
-      <Icon className="h-7" />
+      <Icon className="h-6" />
       <span className="hidden  xl:inline">{text}</span>
     </div>
   );
