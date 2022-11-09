cf17f7461a60f9039a542be3f0d7f0d22c273691
UlfetZakirli <ulfetzakirli@gmail.com>
2022-11-09 15:34:42 +0400

    twitter-app: configured Tailwind CSS and added some styles

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -7,6 +7,7 @@ const Sidebar = () => {
             <div className='flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24'>
                 <Image src="https://rb.gy/ogau5a" width={30}  height={30}/>
             </div>
+            <div></div>
         </div>
     )
 }
