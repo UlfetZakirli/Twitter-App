1e37b186123880a09276087f32d053b7b569dcc9
UlfetZakirli <ulfetzakirli@gmail.com>
2022-11-10 23:51:07 +0400

    twitter-app: emoji-picker installed and added to input component

--- components/Input.jsx
+++ components/Input.jsx
@@ -1,14 +1,14 @@
-import { PhotographIcon, XIcon } from "@heroicons/react/outline";
+import { CalendarIcon, ChartBarIcon, EmojiHappyIcon, PhotographIcon, XIcon } from "@heroicons/react/outline";
 import React, { useRef, useState } from "react";
+import Picker from 'emoji-picker-react';
 
 const Input = () => {
   const [input, setInput] = useState("");
   const [selectedFile, setSelectedFile] = useState(null);
-  const filePickerRef=useRef()
+  const filePickerRef = useRef();
+  const [showEmojis, setShowEmojis] = useState(false)
 
-  const addImageToPost=()=>{
-
-  }
+  const addImageToPost = () => {};
   return (
     <div
       className={`border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll scrollbar-hide`}
@@ -29,22 +29,60 @@ const Input = () => {
              placeholder-gray-500 tracking-wide w-full min-h-[50px] scrollbar-hide "
           />
           {selectedFile && (
-          <div className="relative">
-            <div className="absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26] bg-opacity-75
-             rounded-full flex items-center justify-center top-1 left-1 cursor-pointer" onClick={()=>setSelectedFile(null)}></div>
-          <XIcon className="text-white h-5"/> 
-          </div>
-
+            <div className="relative">
+              <div
+                className="absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26] bg-opacity-75
+             rounded-full flex items-center justify-center top-1 left-1 cursor-pointer"
+                onClick={() => setSelectedFile(null)}
+              ></div>
+              <XIcon className="text-white h-5" />
+            </div>
           )}
-          <img src={selectedFile} alt="" className="rounded-2xl max-h-80 object-contain"/>
+          <img
+            src={selectedFile}
+            alt=""
+            className="rounded-2xl max-h-80 object-contain"
+          />
         </div>
         <div className="flex items-center justify-between pt-2.5">
-            <div className="flex items-center ">
-                <div className="icon" onClick={()=>filePickerRef.current.click()}>
-                    <PhotographIcon className="h-[22px] text-[#1d9bf0]"/>
-                    <input type="file"  ref={filePickerRef} hidden onChange={addImageToPost}/>
-                </div>
+          <div className="flex items-center ">
+            <div className="icon" onClick={() => filePickerRef.current.click()}>
+              <PhotographIcon className="h-[22px] text-[#1d9bf0]" />
+              <input
+                type="file"
+                ref={filePickerRef}
+                hidden
+                onChange={addImageToPost}
+              />
             </div>
+            <div className="icon rotate-90 " >
+                <ChartBarIcon className=" text-[#1d9bf0] h-[22px] " />
+              </div>
+
+              <div className="icon" onClick={() => setShowEmojis(!showEmojis)}>
+                <EmojiHappyIcon className="text-[#1d9bf0] h-[22px]" />
+              </div>
+
+              <div className="icon">
+                <CalendarIcon className="text-[#1d9bf0] h-[22px]" />
+              </div>
+              {
+                showEmojis && (
+                  <div style={{
+                    position: "absolute",
+                    marginTop: "480px",
+                    marginLeft: -40,
+                    borderRadius: "20px",
+                  }}>
+                  <Picker
+               theme="dark"
+               width={300}
+               height={380}
+               />
+               </div>
+                )
+              }
+          </div>
         </div>
       </div>
     </div>
