a8c37292bd2a9eab593f1fc9f5cb6ce04f9d4099
UlfetZakirli <ulfetzakirli@gmail.com>
2022-11-10 12:49:30 +0400

    twitter-app: created body of Feed component and added navbar to it

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
       </Head>
       <main className='bg-black min-h-screen flex max-w-[1500px] mx-auto'>
         <Sidebar />
-        {/* <Feed /> */}
+        <Feed />
         {/* <Widgets /> */}
 
         {/* Modal */}
