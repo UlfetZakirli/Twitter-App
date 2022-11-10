c0885af23bd1454c8c4972e41f1527edbd375860
UlfetZakirli <ulfetzakirli@gmail.com>
2022-11-10 14:31:23 +0400

    twitter-app: image Picker configured, styled and added to input component

--- components/Input.jsx
+++ components/Input.jsx
@@ -1,7 +1,14 @@
-import React, { useState } from "react";
+import { PhotographIcon, XIcon } from "@heroicons/react/outline";
+import React, { useRef, useState } from "react";
 
 const Input = () => {
   const [input, setInput] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+  const filePickerRef=useRef()
+
+  const addImageToPost=()=>{
+
+  }
   return (
     <div
       className={`border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll scrollbar-hide`}
@@ -21,9 +28,23 @@ const Input = () => {
             className="bg-transparent outline-none text-[#d9d9d9] text-lg
              placeholder-gray-500 tracking-wide w-full min-h-[50px] scrollbar-hide "
           />
-          <div>
-            
+          {selectedFile && (
+          <div className="relative">
+            <div className="absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26] bg-opacity-75
+             rounded-full flex items-center justify-center top-1 left-1 cursor-pointer" onClick={()=>setSelectedFile(null)}></div>
+          <XIcon className="text-white h-5"/> 
           </div>
+
+          )}
+          <img src={selectedFile} alt="" className="rounded-2xl max-h-80 object-contain"/>
+        </div>
+        <div className="flex items-center justify-between pt-2.5">
+            <div className="flex items-center ">
+                <div className="icon" onClick={()=>filePickerRef.current.click()}>
+                    <PhotographIcon className="h-[22px] text-[#1d9bf0]"/>
+                    <input type="file"  ref={filePickerRef} hidden onChange={addImageToPost}/>
+                </div>
+            </div>
         </div>
       </div>
     </div>
