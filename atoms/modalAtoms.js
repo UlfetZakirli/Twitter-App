9ca3fcf60c7510b75925ddd134853a33f62e0da8
UlfetZakirli <ulfetzakirli@gmail.com>
2022-11-13 18:44:57 +0400

    twitter-app: implemented Recoil.js and react-moment

---
+++ atoms/modalAtoms.js
@@ -0,0 +1,12 @@
+import { atom } from "recoil";
+
+
+export const modalState=atom({
+    key:'modalState',
+    default:false
+})
+
+export const postIdState=atom({
+    key:"postIdState",
+    default:''
+})
--- components/Post.jsx
+++ components/Post.jsx
@@ -23,7 +23,9 @@ import {
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-
+import Moment from "react-moment";
+import { useRecoilState } from "recoil";
+// import { modalState, postIdState } from "../atoms/modalAtom";
 import { db } from "../firebase";
 
 const Post = ({ id, post, postPage }) => {
@@ -79,7 +81,23 @@ const Post = ({ id, post, postPage }) => {
          alt=""
           />
           <div className={`text-[#6e767d] flex justify-between w-10/12 ${postPage && 'mx-auto'}`}>
-        
+          {/* <div
+            className="flex items-center space-x-1 group"
+            onClick={(e) => {
+              e.stopPropagation();
+              setPostId(id);
+              setIsOpen(true);
+            }}
+          >
+            <div className="icon group-hover:bg-[#1d9bf0] group-hover:bg-opacity-10">
+              <ChatIcon className="h-5 group-hover:text-[#1d9bf0]" />
+            </div>
+            {comments.length > 0 && (
+              <span className="group-hover:text-[#1d9bf0] text-sm">
+                {comments.length}
+              </span>
+            )}
+          </div> */}
 
           {session.user.uid === post?.id ? (
             <div
@@ -102,7 +120,30 @@ const Post = ({ id, post, postPage }) => {
             </div>
           )}
 
-       
+          {/* <div
+            className="flex items-center space-x-1 group"
+            onClick={(e) => {
+              e.stopPropagation();
+              likePost();
+            }}
+          >
+            <div className="icon group-hover:bg-pink-600/10">
+              {liked ? (
+                <HeartIconFilled className="h-5 text-pink-600" />
+              ) : (
+                <HeartIcon className="h-5 group-hover:text-pink-600" />
+              )}
+            </div>
+            {likes.length > 0 && (
+              <span
+                className={`group-hover:text-pink-600 text-sm ${
+                  liked && "text-pink-600"
+                }`}
+              >
+                {likes.length}
+              </span>
+            )}
+          </div> */}
 
           <div className="icon group">
             <ShareIcon className="h-5 group-hover:text-[#1d9bf0]" />
