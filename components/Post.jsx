fde6534982af40b80def6370e3ad0be246c3314f
UlfetZakirli <ulfetzakirli@gmail.com>
2022-11-13 14:44:45 +0400

    twitter-app: Post(tag,userImage,text) displayed on UI

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { DotsHorizontalIcon } from "@heroicons/react/outline";
+import React from "react";
 
-const Post = ({id,post}) => {
+const Post = ({ id, post, postPage }) => {
   return (
-    <div>Post</div>
-  )
-}
+    <div className="p-3 flex cursor-pointer border-b border-gray-700">
+      {!postPage && (
+        <img
+          src={post?.userImg}
+          alt=""
+          className="w-11 h-11 rounded-full mr-4"
+        />
+      )}
+      <div className="flex flex-col space-y-2 w-full">
+        <div className={`flex ${!postPage && "justify-between"}`}>
+          {postPage && (
+            <img
+              src={post?.userImg}
+              alt="Profile Picture"
+              className="w-11 h-11 rounded-full mr-4"
+            />
+          )}
+          <div className="text-[#6e767d]">
+            <div className="inline-block group">
+              <h4 className={`font-bold text-[15px] sm:text-base
+               text-[#d9d9d9] group-hover:underline ${!postPage && 'inline-block'}`}>{post?.username}</h4>
+              <span className={`text-sm sm:text-[15px] ${!postPage && "ml-1.5"}`}>@{post?.tag}</span>
+            </div>{" "}
+            ·{" "}
+            <span className="hover:underline text-sm sm:text-[15px]">
+            {/* <Moment fromNow>{post?.timestamp?.toDate()}</Moment> */}
+            </span>
+            {!postPage && (
+              <p className="text-[#d9d9d9] text-[15px] sm:text-base mt-0.5">
+                {post?.text}
+              </p>
+            )}
+          </div>
+          <div className="icon group flex-shrink-0 ml-auto">
+            <DotsHorizontalIcon className="h-5 text-[#6e767d] group-hover:text-[#1d9bf0]"/>
 
-export default Post
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
