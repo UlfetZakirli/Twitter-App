2568f2d82f1615dbe7859c84b4436dc9d6d7a9fa
UlfetZakirli <ulfetzakirli@gmail.com>
2022-11-16 12:00:31 +0400

    twitter-app: Trending component completed✅

--- components/Feed.js
+++ components/Feed.js
@@ -10,7 +10,7 @@ const Feed = () => {
   const [posts, setPosts] = useState([])
 
 
-  // CLEAN
+
   useEffect(
     () =>
       onSnapshot(
@@ -22,7 +22,7 @@ const Feed = () => {
     [db]
   );
   return (
-    <div className="text-white flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
+    <div className="text-white flex-grow border-l border-r border-gray-700 max-w-xl sm:ml-[73px] xl:ml-[370px]">
       <div className="text-[#d9d9d9] flex items-center sm:justify-between
        py-2 px-3 sticky top-0 z-50 bg-black border-b border-gray-700">
         <h2 className="text-lg sm:text-xl font-bold">Home</h2>
--- components/Widgets.jsx
+++ components/Widgets.jsx
@@ -1,36 +1,39 @@
 import { SearchIcon } from "@heroicons/react/outline";
-import React from 'react'
+import React from "react";
 import Trending from "./Trending";
 
-const Widgets = ({trendingResults,followResults}) => {
+const Widgets = ({ trendingResults, followResults }) => {
   return (
-    <div className='hidden lg:inline ml-8 xl:w-[450px] py-1 space-y-5'> 
-    <div className="sticky top-0 py-1.5 bg-black z-50 w-11/12 xl:w-9/12">
-      <div className="flex items-center bg-[#202327] p-3 rounded-full relative">
-        
-      <SearchIcon className='text-gray-500 h-5 z-50'/>
-      <input
-      type="text"
-      className="bg-transparent 
-      placeholder-gray-500 outline-none
-      text-[#d9d9d9] absolute inset-0 pl-11 border 
-      border-transparent w-full"
-      placeholder="Search Twitter"
-      />
+    <div className="hidden lg:inline ml-8 xl:w-[650px] py-1 space-y-5">
+      <div className="sticky top-0 py-1.5 bg-black z-50 w-11/12 xl:w-9/12">
+        <div className="flex items-center bg-[#202327] p-3 rounded-full relative">
+          <SearchIcon className="text-gray-500 h-5 z-50" />
+          <input
+            type="text"
+            className="bg-transparent 
+             placeholder-gray-500 outline-none
+             text-[#d9d9d9] absolute inset-0 pl-11 border 
+             border-transparent w-full"
+            placeholder="Search Twitter"
+          />
+        </div>
+      </div>
+      <div className="text-[#d9d9d9] space-3 bg-[#15181c] pt-2 rounded-xl w-11/12 xl:w-9/12">
+        <h4 className="font-bold text-xl px-4">What's happening</h4>
+
+        {trendingResults.map((result, index) => (
+          <Trending key={index} result={result} />
+        ))}
+        <button
+          className="hover:bg-white hover:bg-opacity-[0.03] px-4 py-3 
+          cursor-pointer transition duration-200 ease-out flex items-center
+          justify-between w-full text-[#1d9bf0] font-light"
+        >
+          Show more
+        </button>
       </div>
     </div>
-    <div className="text-[#d9d9d9] space-3 bg-[#15181c] pt-2 rounded-xl w-11/12 xl:w-9/12">
-      <h4 className="font-bold text-xl px-4">What's happening</h4>
+  );
+};
 
-{trendingResults.map((result,index)=>(
-  <Trending key={index} result={result}/>
-))}
-<button className="hover:bg-white hover:bg-opacity-[0.03] px-4 py-3 
-cursor-pointer transition duration-200 ease-out flex items-center
- justify-between w-full text-[#1d9bf0] font-light">Show more</button>
-    </div>
-    </div>
-  )
-}
-
-export default Widgets
+export default Widgets;
